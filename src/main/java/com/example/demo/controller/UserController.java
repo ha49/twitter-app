4503ce25e30e908da36fa550f413ffa4ed279012
ha49 <kaysersoza@outlook.com>
2021-02-11 12:11:12 +0100

    added UserController containing one post and one get method.

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/account")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/signup")
+    public User addNewUser(@RequestBody User user) {
+
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/get/{id}")
+
+    public User getUser(@PathVariable long id) {
+
+
+        return userRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("user with id " + id + " does not exist ")
+
+
+        );
+    }
+}
